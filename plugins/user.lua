ec1189187c81950c92a240fa9840e90246e0d65a
fukuda <3498972895@qq.com>
2024-03-09 11:08:09 +0800

    On branch main

--- plugins/user.lua
+++ plugins/user.lua
@@ -25,5 +25,18 @@ return {
         }
       end,
     },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = {
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = false,
+            hide_gitignored = true,
+          },
+        },
+      },
+    },
   },
 }
