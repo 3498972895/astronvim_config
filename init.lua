12198c6b2b4f87b121eaad07ab8be59493b72806
fukuda <3498972895@qq.com>
2024-03-23 14:25:04 +0800

    Changes to be committed: modified:   init.lua modified:   plugins/community.lua

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ return {
 
   -- Set colorscheme to use
   -- colorscheme = "catppuccin-macchiato",
-  colorscheme = "monokai-pro",
+  colorscheme = "dracula",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
